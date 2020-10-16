b45a4c25df827d0ce0e49677ee8eaaf8e88361f8
ctbarrel <ctbarrel@gmail.com>
2020-10-16 15:39:43 -0600

    build script

---
+++ public/precache-manifest.3c40a39620ab204936a02e8c0aa7357e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90cf6d1093c20ec3e428333dcfb087f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a9afab873e75cd16dc5",
+    "url": "/static/css/main.049b104f.chunk.css"
+  },
+  {
+    "revision": "92b6203e4be6d86605fe",
+    "url": "/static/js/2.566e871b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.566e871b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a9afab873e75cd16dc5",
+    "url": "/static/js/main.097d7f48.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
